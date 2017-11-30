9567d22f0337c6e70e588f6ad6b05ab7c7954906
macro <869158081@qq.com>
2017-11-30 13:02:43 +0800

    Update ComstomerDao.java
    继承HibernateDaoSupport

--- src/main/java/com/niydiy/dao/impl/ComstomerDao.java
+++ src/main/java/com/niydiy/dao/impl/ComstomerDao.java
@@ -1,12 +1,13 @@
 package com.niydiy.dao.impl;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 import com.niydiy.dao.CustomerDao;
 import com.niydiy.domain.Customer;
 
-public class ComstomerDao implements CustomerDao {
-	private HibernateTemplate hibernateTemplate;
+public class ComstomerDao extends HibernateDaoSupport implements CustomerDao {
+	/×private HibernateTemplate hibernateTemplate;
 
 	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
 		this.hibernateTemplate = hibernateTemplate;
@@ -14,5 +15,8 @@ public class ComstomerDao implements CustomerDao {
 
 	public void save(Customer customer) {
 		hibernateTemplate.save(customer);
+	}×/
+	public void save(Customer customer) {
+		this.getHibernateTemplate().save(customer);
 	}
 }
